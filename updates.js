8405244160dab8fd9baf6b3cf1112581bb954606
ItsNovrix <itsnovrix@hotmail.com>
2026-01-08 08:07:57 -0500

    Add updates.js with latest updates and changes

---
+++ updates.js
@@ -0,0 +1,105 @@
+window.UPDATES = {
+"title": "Latest from the WSQK Archivist",
+  "items": [
+    {
+      "headline": "Bug, Issue, Suggestion?",
+      "details": `â€£ Send a DM to one of my socials
+      â€£ Discord: livinghuman
+      â€£ Reddit: Living_Human
+      â€£ BlueSky: livinghuman.bsky.social`
+    },
+    {
+      "headline": "Shoutouts - Thanks for sharing the word :)",
+      "details": `â€£ MaddieHenne
+      â€£ Caitslolx
+      â€£ JustinH84
+      â€£ Daafdemandoetmee - For sharing the 1-1 full stream`
+    },
+    {
+      "headline": "1-3-26 - Updates",
+      "details": `â€£ Added test signal for 11-24
+      â€£ Added partial archive for 11-24 and 11-25
+      â€£ Changed dates to start from 12am instead of 6am
+      â€£ If anyone has read these, what a ride it has been over the last month.
+      â€£ Your WSQK Archivist signing off.`
+    },
+    {
+      "headline": "1-2-26 - Updates",
+      "details": `â€£ Added partial archive for 11-26
+      â€£ Finished archive for 11-27
+      â€£ Added archive for 1-1 | 12am to 11pm
+      â€£ Added download button for each timeslot`
+    },
+    {
+      "headline": "12-31-25 - Updates",
+      "details": `â€£ Added archive for 12-30 | 12am to 6am
+      â€£ Added archive for 12-31 | 6am to 12am`
+    },
+    {
+      "headline": "12-30-25 - Updates",
+      "details": `â€£ Added archive for 12-28 | 4pm to 6am
+      â€£ Added archive for 12-29 | 6am to 6am
+      â€£ Added archive for 12-30 | 6am to 12am`
+    },
+    {
+      "headline": "12-28-25 - Updates",
+      "details": `â€£ Added archive for 12-26 | 4pm to 6am
+      â€£ Added archive for 12-27 | 6am to 6am
+      â€£ Added archive for 12-28 | 6am to 4pm`
+    },
+    {
+      "headline": "12-26-25 - Updates",
+      "details": `â€£ Added archive for 12-24 | 4pm to 6am
+      â€£ Added archive for 12-25 | 6am to 6am
+      â€£ Added archive for 12-26 | 6am to 4pm`
+    },
+    {
+      "headline": "12-24-25 - Updates",
+      "details": `â€£ Added archive for 12-22 | 4pm to 6am
+      â€£ Added archive for 12-23 | 6am to 6am
+      â€£ Added archive for 12-24 | 6am to 4pm`
+    },
+    {
+      "headline": "12-22-25 - Updates",
+      "details": `â€£ Added archive for 12-19 | 4pm to 6am
+      â€£ Added archive for 12-20 | 6am to 6am
+      â€£ Added archive for 12-21 | 6am to 6am
+      â€£ Added archive for 12-22 | 6am to 4pm`
+    },
+    {
+      "headline": "12-19-25 - Updates",
+      "details": `â€£ Added archive for 12-17 | 4pm to 6am
+      â€£ Added archive for 12-18 | 6am to 6am
+      â€£ Added archive for 12-19 | 6am to 4pm`
+    },
+    {
+      "headline": "12-17-25 - Updates",
+      "details": `â€£ Added archive for 12-14 | 4pm to 6am
+      â€£ Added archive for 12-15 | 6am to 6am
+      â€£ Added archive for 12-16 | 6am to 6am
+      â€£ Added archive for 12-17 | 6am to 4pm`
+    },
+    {
+      "headline": "12-14-25 - Updates",
+      "details": `â€£ Added archive for 12-13 | 9pm to 6am
+      â€£ Added archive for 12-14 | 6am to 4pm`
+    },
+    {
+      "headline": "12-13-25 - Updates",
+      "details": `â€£ Added update icon (this page) 
+      â€£ Added archive for 12-12 | 6am to 6am
+      â€£ Added archive for 12-13 | 6am to 9pm`
+    },
+    {
+      "headline": "12-12-25 - Mobile Update",
+      "details": `â€£ Reworked mobile layout for better end user experience 
+      â€£ Updated CarPlay detection to display correct data
+      â€£ Tried to implement auto restart after disruption, this is still in testing`
+    },
+    {
+      "headline": "12-11-25 - Hosting Update",
+      "details": `â€£ Moved hosting off GitHub to a private provider 
+      â€£ This should improve reliability with larger file storage and bandwidth`
+    }
+  ]
+};
