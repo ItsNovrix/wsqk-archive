190900c9374a05ee7b0ebcb2a7df2e81b4fdc67e
ItsNovrix <itsnovrix@hotmail.com>
2026-01-08 08:09:12 -0500

    Add alert messages with titles and audio cues

---
+++ alerts.js
@@ -0,0 +1,21 @@
+window.ALERTS = [
+  {
+    title: "Station Breach",
+    message: "The tunnels have been compromised. Evacuate immediately.",
+    audioCue: "./effects/demogorg.mp3"
+  },
+  {
+    title: "ERR TOWER LOST",
+    message: "POWER FROM TOWER LOST. PLEASE RESTART VIA THE GRAY? RED? DIAL BY TURNING IT UP.",
+    audioCue: "./effects/powerdown.mp3"
+  },
+  {
+    title: "Upside Down",
+    message: "Rockin Robin Reporting that the Upside Down will be crawling tonight."
+  },
+  {
+    title: "ERR CONNECTION LOST",
+    message: "WATTAGE OVERLOAD. SYSTEM SHUTTING DOWN.",
+    audioCue: "./effects/overload.wav"
+  }
+];
